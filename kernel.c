edf0cf28e9e0c6805924deb06c593e139b23d019
Bailey Harrison <baileyharrison04@outlook.com>
2021-04-29 22:33:22 +0100

    Load the entire 16 KB kernel

--- kernel.c
+++ kernel.c
@@ -3,14 +3,14 @@ void video_write_str(int x, int y, char* str);
 
 void _start()
 {
-	video_write_str(0, 0, "finally 32 bit and C are working");
+	video_write_str(0, 3, "finally 32 bit and C are working");
 	for ( ;; ) ;	// stop the CPU from executing random RAM
 }
 
 void video_write_char(int x, int y, char c)
 {
 	char* ptr = (char*)0xB8000;	// start of VRAM
-	ptr += y * 80 + x * 2;
+	ptr += (y * 160) + (x * 2);
 	*ptr = c;
 	ptr++;
 	*ptr = 0x0A;
